472b39e242ba037cb472ee1a7586cbd4423bf78b
shantanuatgit <shantanu3250@gmai..com>
2021-03-15 13:08:07 +0530

    configured postgresql db

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -6,7 +6,7 @@ from django.core.management.base import BaseCommand
 
 
 class Command(BaseCommand):
-    """Django command to pause exection until database available"""
+    """Django command to pause the exection until database available"""
     def handle(self, *args, **options):
         self.stdout.write('Waiting for the database...')
         db_conn = None
